8ad19dc7d7cc5e9b9bcf22d761560a696f4a9f98
Or Adar <orniky91@gmail.com>
2025-02-09 15:28:47 +0200

    Update CarDealership.java

--- CarDealership.java
+++ CarDealership.java
@@ -1,78 +1,99 @@
 package carDealership;
-// Assignment: 5
 // Author: Or Adar, ID: 305468506
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * The CarDealership class represents a simple dealership management system.
+ * It allows users to view employees, check unsold cars, register sales, and add new cars.
+ */
 public class CarDealership {
-    public static<T extends Comparable<T>> void sort(ArrayList<T> list){
-        int size= list.size();
+    
+    /**
+     * Generic sorting method using Bubble Sort (can be optimized with Collections.sort()).
+     *
+     * @param list The ArrayList of Comparable elements to sort.
+     * @param <T>  A type that implements Comparable.
+     */
+    public static <T extends Comparable<T>> void sort(ArrayList<T> list) {
+        int size = list.size();
         T temp;
-        for(int i=0;i<size-1;i++){
-            for(int j=0;j<size-1;j++){
-                if(list.get(j).compareTo(list.get(j+1))>0){
-                    temp=list.get(j);
-                    list.set(j,list.get(j+1));
-                    list.set(j+1,temp);
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
                 }
             }
         }
     }
 
+    /**
+     * Main method to run the Car Dealership management system.
+     *
+     * @param args Command-line arguments (not used).
+     * @throws Exception Handles incorrect user input.
+     */
     public static void main(String[] args) throws Exception {
-        Path sold=Paths.get("C:\\Users\\ornik\\IdeaProjects\\FifthHW\\src\\carDealership\\Sold.txt");
-        //Files.createFile(sold);
+        
+        // File paths for storing data
+        Path sold = Paths.get("Sold.txt");
+        ArrayList<Car> carList = new ArrayList<>();
+        Path fileCars = Paths.get("CarDealership.txt");
 
-        ArrayList<Car> carList=new ArrayList<>();
-        Path fileCars= Paths.get("C:\\Users\\ornik\\IdeaProjects\\FifthHW\\src\\carDealership\\CarDealership.txt");
-        String carContent=Files.readString(fileCars);
-        String[] lines=carContent.split("\n");
-        for(int i=0;i< lines.length;i++){
-            String[] values=lines[i].split(" ");
+        // Load cars from file
+        String carContent = Files.readString(fileCars);
+        String[] lines = carContent.split("\n");
+        for (String line : lines) {
+            String[] values = line.split(" ");
             try {
-                Car car = new Car(values[0], Integer.parseInt(values[1]),
-                        values[2], Integer.parseInt(values[3]), Integer.parseInt(values[4]));
+                Car car = new Car(values[0], Integer.parseInt(values[1]), values[2], Integer.parseInt(values[3]), Integer.parseInt(values[4]));
                 carList.add(car);
-            }
-            catch(Exception e){
-                System.out.println("Can't add a car with incorrect values");
+            } catch (Exception e) {
+                System.out.println("Invalid car entry: " + e.getMessage());
             }
         }
-        ArrayList<Employee> employeeList=new ArrayList<>();
-        Path fileEmployees=Paths.get("C:\\Users\\ornik\\IdeaProjects\\FifthHW\\src\\carDealership\\Employee.txt");
-        String employeeContent=Files.readString(fileEmployees);
-        String[] lines2=employeeContent.split("\n");
-        for(int i=0;i< lines2.length;i++){
-            String[] values=lines2[i].split(" ");
+
+        // Load employees from file
+        ArrayList<Employee> employeeList = new ArrayList<>();
+        Path fileEmployees = Paths.get("Employee.txt");
+        String employeeContent = Files.readString(fileEmployees);
+        String[] lines2 = employeeContent.split("\n");
+        for (String line : lines2) {
+            String[] values = line.split(" ");
             try {
-               Employee employee = new Employee(values[0],values[1],Integer.parseInt(values[2]));
-               employeeList.add(employee);
-            }
-            catch(Exception e){
-                System.out.println("Can't add a employee with incorrect values");
+                Employee employee = new Employee(values[0], values[1], Integer.parseInt(values[2]));
+                employeeList.add(employee);
+            } catch (Exception e) {
+                System.out.println("Invalid employee entry: " + e.getMessage());
             }
         }
-        System.out.println(employeeList);
+
+        Scanner scan = new Scanner(System.in);
         int choice;
-        Scanner scan=new Scanner(System.in);
-        while(true){
+
+        while (true) {
             Menu.printMenu();
-            choice=scan.nextInt();
-            switch(choice){
+            choice = scan.nextInt();
+            
+            switch (choice) {
                 case 1:
+                    // Sort employees by number of sales
                     sort(employeeList);
-                    System.out.println("list of employees:");
+                    System.out.println("List of employees:");
                     for (Employee employee : employeeList) {
                         System.out.println(employee);
                     }
                     break;
                 case 2:
-                    System.out.println("cars in the agency that have yet to be sold:");
+                    // Display unsold cars
+                    System.out.println("Cars available for sale:");
                     for (Car car : carList) {
                         if (!Files.readString(sold).contains(car.getCarID())) {
                             System.out.println(car);
@@ -80,86 +101,48 @@ public class CarDealership {
                     }
                     break;
                 case 3:
-                    System.out.println("List of employees:");
-                    for (Employee employee : employeeList) {
-                        System.out.println(employee.getName() + " - " + employee.getID());
-                    }
-
-                    System.out.print("Enter employee ID: ");
-                    String selectedEmployeeID = scan.next();
-
+                    // Process a car sale
+                    System.out.println("Enter Employee ID:");
+                    String empID = scan.next();
                     Employee selectedEmployee = null;
-                    for (Employee employee : employeeList) {
-                        if (employee.getID().equals(selectedEmployeeID)) {
-                            selectedEmployee = employee;
+                    for (Employee e : employeeList) {
+                        if (e.getID().equals(empID)) {
+                            selectedEmployee = e;
                             break;
                         }
                     }
-
-                    if (selectedEmployee == null) {
-                        throw new Exception("Incorrect ID");
-                    }
-
-                    System.out.println("Unsold cars:");
-                    for (Car car : carList) {
-                        if (!Files.readString(sold).contains(car.getCarID())) {
-                            System.out.println(car);
-                        }
-                    }
-
-                    System.out.print("Enter car ID: ");
-                    String selectedCarNumber = scan.next();
-
+                    if (selectedEmployee == null) throw new Exception("Invalid Employee ID");
+                    
+                    System.out.println("Enter Car ID to sell:");
+                    String carID = scan.next();
                     Car selectedCar = null;
-                    for (Car car : carList) {
-                        if (car.getCarID().equals(selectedCarNumber)) {
-                            selectedCar = car;
+                    for (Car c : carList) {
+                        if (c.getCarID().equals(carID)) {
+                            selectedCar = c;
                             break;
                         }
                     }
-
-                    if (selectedCar == null) {
-                        throw new Exception("Incorrect car ID");
-                    }
-
+                    if (selectedCar == null) throw new Exception("Invalid Car ID");
+                    
                     selectedEmployee.carSale(selectedCar, sold);
                     carList.remove(selectedCar);
-
-                    System.out.println("Sale done successfully");
-
+                    System.out.println("Sale completed!");
                     break;
-
                 case 4:
-                    System.out.print("Enter car ID: ");
-                    String carNumber = scan.next();
-
-                    System.out.print("Enter manufactured year: ");
-                    int year = scan.nextInt();
-
-                    System.out.print("Enter manufactur name:");
-                    String manufacturer = scan.next();
-
-                    System.out.print("Enter KM:");
-                    int km = scan.nextInt();
-
-                    System.out.print("Enter price:");
-                    int price = scan.nextInt();
-
+                    // Add a new car to inventory
+                    System.out.println("Enter Car Details (ID, Year, Manufacturer, KM, Price):");
                     try {
-                        Car newCar = new Car(carNumber, year, manufacturer, km, price);
+                        Car newCar = new Car(scan.next(), scan.nextInt(), scan.next(), scan.nextInt(), scan.nextInt());
                         carList.add(newCar);
-                        System.out.println("Car added Successfully!");
+                        System.out.println("Car added successfully!");
                     } catch (Exception e) {
-                        System.out.println(e.getMessage());
+                        System.out.println("Invalid input: " + e.getMessage());
                     }
-
                     break;
-                    default:
-                    System.out.println("Wrong input try again.");
+                default:
+                    System.out.println("Invalid choice. Try again.");
                     break;
-
             }
         }
     }
-
 }
